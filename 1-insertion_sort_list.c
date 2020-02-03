286e292beb7da654fd2cf6fe2359870a6708d717
cristian0497 <cristian04.97@hotmail.com>
2020-02-03 11:18:23 -0500

    [ADD] Betty ok, Algorithm insertion sort in D. Linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - algorithm sorter
+ * @list: Doubl linked list
+ * Return: Void Function
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *aux, *back, *nxt, *head = (*list);
+
+	while (head != NULL && head->next)
+	{
+		if (head->n > head->next->n)
+		{
+			back = head->prev;
+			nxt = head->next;
+			if (!back)
+			{
+				nxt->prev = NULL;
+				(*list) = nxt;
+			}
+			else
+			{
+				nxt->prev = back;
+				back->next = nxt;
+			}
+			aux = nxt->next;
+			if (!aux)
+			{
+				head->next = NULL;
+				nxt->next = head;
+			}
+			else
+			{
+				nxt->next = head;
+				aux->prev = head;
+			}
+			head->next = aux;
+			head->prev = nxt;
+			print_list((*list));
+			head = (*list);
+		}
+		else
+			head = head->next;
+	}
+}
